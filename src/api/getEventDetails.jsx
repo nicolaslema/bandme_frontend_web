2d98e6dc0df56494e59f854520874d583cdb86eb
nicolaslema <nicolemadev@gmail.com>
2022-11-02 23:31:15 -0300

    first Commit

--- src/api/getEventDetails.jsx
+++ src/api/getEventDetails.jsx
@@ -1,1 +1,45 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 
+
+const useFetch = (url) =>{
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(null);
+    const [error, setError] = useState(null);
+
+
+
+useEffect(()=>{
+    const dataFetching = async () =>{
+
+        try {
+            
+            const response = await axios.get(url);
+            const newData = response.data.data;
+            setData(newData);
+            console.log(newData);
+            setError(null);
+        } catch (err) {
+            setError(err.message);
+            setData(null);
+            
+        }finally{
+            setLoading(false)
+        }
+    };
+
+    dataFetching();
+
+}, [url]);
+
+return{
+    data,
+    loading,
+    error
+};
+
+
+
+};
+
+export default useFetch;
--- src/components/eventDetails/EventDetails.jsx
+++ src/components/eventDetails/EventDetails.jsx
@@ -1,28 +1,25 @@
 import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom';
 
+
 const EventDetails = () => {
   
   let{id} = useParams()
   const [eventInfo, setEventInfo] = useState([]);
+  const [title, setTitle] = useState(null);
   const url = `https://bandme-dashboard-api.herokuapp.com/api/v1/dashboard/get-post-details?posteoId=${id}`
-  
-  useEffect(()=>{
 
-    const fetchPostbyId =  async(id)=>{
-      const response = await fetch(url);
-      const data = await response.json()
-      setEventInfo(data.data);
-    
-    }
-     fetchPostbyId(id);
+  
+  
+ 
   
 
-  },  [])
+ 
+
+
   
-
-
-
+//TODO: 
+//Funcion para parsear url + id 
 
   return (
     <div>
@@ -35,20 +32,20 @@ const EventDetails = () => {
     </div>
     <div class="flex flex-col w-full md:flex-row">
         <div class="flex flex-row justify-around p-4 font-bold leading-none text-gray-800 uppercase bg-bandme_orange rounded md:flex-col md:items-center md:justify-center md:w-1/4">
-          <div class="md:text-3xl">{eventInfo.date}</div>
+          <div class="md:text-3xl">{}</div>
             
-            <div class="md:text-xl">{eventInfo.time}</div>
+            <div class="md:text-xl">{}</div>
         </div>
         <div class="p-4 font-normal text-gray-800 md:w-3/4">
-            <h1 class="mb-4 text-4xl font-bold leading-none tracking-tight text-gray-800"> {eventInfo.title}</h1>
-            <p class="leading-normal">{eventInfo.description}</p>
+            <h1 class="mb-4 text-4xl font-bold leading-none tracking-tight text-gray-800"> {}</h1>
+            <p class="leading-normal">{}</p>
             <div class="flex flex-row items-center mt-4 text-gray-700">
                 <div class="w-1/2">
-                    {eventInfo.street} - {eventInfo.street_number}
+                    {} - {}
                 </div>
                 <div class="w-1/2 flex justify-end items-center">
-                    <img className='w-24 h-24 rounded-full' src={eventInfo.profile_photo} alt="" class="w-8"></img>
-                    <p className='ml-6'>{eventInfo.first_name} {eventInfo.last_name}</p>
+                    <img className='w-24 h-24 rounded-full'  alt="" class="w-8"></img>
+                    <p className='ml-6'>{} {}</p>
                 </div>
             </div>
         </div>
