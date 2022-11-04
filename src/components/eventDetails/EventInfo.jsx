97d33b33dc9a9fd20adb5d79c054e0eb7aa2ed19
nicolaslema <nicolemadev@gmail.com>
2022-11-04 02:57:23 -0300

    added: custom css file config

--- src/components/eventDetails/EventInfo.jsx
+++ src/components/eventDetails/EventInfo.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { useEffect } from 'react'
+import './EventDetails.css'
+
+
 
 const EventInfo = ({
   title, 
@@ -11,80 +15,54 @@ const EventInfo = ({
   first_name,
   last_name}) => {
 
+    useEffect(()=>{
+  
+    })
 
   return (
-    <div>
+    <div className="bg-white flex-col ">
+<img className='h-72  w-screen' src="https://images.unsplash.com/photo-1470229722913-7c0e2dbbafd3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29uY2VydHxlbnwwfHwwfHw%3D&w=1000&q=80" alt="" />
+    <div className='bg-white relative main  w-screen  '>
+  <div className="poly-1  absolute bg-blue-500  fun w-2/4 h-42 justify-start items-start  "> 
+  
+  
+  </div>
+  <h1 className='text-8xl font-bold font-Poppins titulo-evento '>{title}</h1>
 
-<div className="mx-auto bg-bandme_dark  h-screen flex items-center justify-center px-8 ">
-
-<div className='bg-slate-100 shadow-2xl w-screen h-3/4 rounded-br-2xl '>
-
-  <div className='flex-1 shadow-2xl'>
-
-    <div className='flex items-center justify-center bg-slate-100 shadow-2xl'>
-      <img className='w-28  rounded-full items-center justify-center -m-5' src={profile_photo} alt="profilePicture"></img>
+<div class=" bg-white py-24 flex items-center justify-center ">
+    
+    <div className="flex-col items-center justify-center  ">
+    <img src={profile_photo} alt="profile_picture" className='w-24 rounded-full' />
+    <h1 className='font-bold font-Poppins'>{first_name}  {last_name}</h1>
     </div>
+    
+
+    
+</div>
+    
+
+</div>
 
 
-    <div className='flex items-center justify-center bg-slate-100 mb-2 '>
-      <h1 className='text-md  font-Poppins mt-8  overline  decoration-gray-400 decoration-2 underline-offset-8 decoration-wavy tracking-wide'>{first_name} {last_name}</h1>
-    </div>
-
-
-    <div className='flex items-center justify-center bg-bandme_orange p-8 '>
-      <h1 className='text-3xl text-bandme_dark uppercase font-Poppins font-bold mt-8 '>{title}</h1>
-    </div>
-
-    </div>
-
-    <div className=' flex-col items-center justify-center '>
-      
-   
-   <div className='flex '> 
-   <div className=' flex-col  w-2/4 h-2/4 bg-bandme_orange shadow-2xl'>
-      <p className='text-2xl font-Poppins font-bold text-center text-slate-800 '>{street}</p>
-      <p className='text-2xl font-Poppins font-bold text-center text-slate-800 '>{street_number}</p>
-    </div>
-    <div className=' flex-col  w-2/4 h-2/4 bg-bandme_orange shadow-2xl'>
-      <p className='text-2xl font-Poppins font-bold uppercase text-center text-slate-800 '>{date}</p>
-      <p className='text-2xl font-Poppins font-bold uppercase text-center text-slate-800 '>{time}</p>
-    </div>
-
-    </div>
-
-    <p className='text-lg font-Poppins '>{description}</p>
-    </div>
-
-   
-
-
-
+<div className='  flex-wrap flex justify-center py-8 gap-24'>
  
-   
 
+  <h1 className='text-2xl font-bold font-Poppins titulo-evento '>{street} - {street_number}</h1>
+  <h1 className='text-2xl font-bold font-Poppins titulo-evento '>{date}</h1>
+  <h1 className='text-2xl font-bold font-Poppins titulo-evento '>{time}/hs</h1>
+  <h1 className='text-2xl font-font-Poppins titulo-evento  m-8'>
+  Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+  Tempore modi in ab, corporis ducimus suscipit fugiat nulla impedit laboriosam 
+  reiciendis aperiam nihil blanditiis iure quia dicta nostrum adipisci qui assumenda?
+  </h1>
 </div>
 
 
 
 
-
 </div>
 
 
-
-
-
-
-
-
-
-
-
-
-    
-    
-
-   </div>
   )
 }
 
